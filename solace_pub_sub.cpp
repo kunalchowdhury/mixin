ed2418a591289203be2fead51c1be50c7261e0a3
Kunal Chowdhury <kunalkumar.chowdhury@gmail.com>
2016-08-18 10:39:45 +0530

    Update solace_pub_sub.cpp

--- solace_pub_sub.cpp
+++ solace_pub_sub.cpp
@@ -2,10 +2,20 @@
 // Created by Kunal Chowdhury on 8/17/2016.
 //
 
+#include <iostream>
+#include <thread>
 #include "solace_pub_sub.h"
+#include <chrono>
+static inline void spinlock(volatile int *lock) { while(!__sync_bool_compare_and_swap(lock, 0, 1)) { sched_yield(); } }
+
+static inline void spinunlock(volatile int *lock) { *lock = 0; }
 
 solace_pub_sub::solace_pub_sub(const string &hostname, const string &user_name, const string &vpn_name) : hostname(
-        hostname), user_name(user_name), vpn_name(vpn_name) {}
+        hostname), user_name(user_name), vpn_name(vpn_name)
+{
+    _lock = 0;
+    _cur = 0;
+}
 
 solace_pub_sub::~solace_pub_sub() {
 
@@ -25,4 +35,12 @@ void solace_pub_sub::subscribe(string topic) {
 
 void solace_pub_sub::unsubscribe(string topic) {
 
+}
+
+void solace_pub_sub::inc() {
+    spinlock(&_lock);
+    long t = std::chrono::high_resolution_clock::now().time_since_epoch().count()/1000000000  ;
+    ++_cur;
+    std::cout << " thread id value -> " << this_thread::get_id() << " -- " << _cur << " @ " << t << " \n" ;
+    spinunlock(&_lock);
 }
